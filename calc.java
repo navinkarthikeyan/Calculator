548a13e9d179f4bb7eb20a8cb909d9b3df3783f0
Exyernos <Exyernos@gmail.com>
2022-07-19 13:15:10 +0530

    added new arithmetic operation to calc

--- calc.java
+++ calc.java
@@ -3,19 +3,32 @@ import java.util.Scanner;
 public class calc {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System. in);
-        System.out.println("You want to add or sub <3: ");
+        System.out.println("You Want To Perfrom add or sub or mult or div: ");
         String j = sc.nextLine();
         if (j.equals("add")) {
-            int a = sc.nextInt();
-            int b = sc.nextInt();
-            int c = a + b;
-            System.out.println(c);
+            int sum_a   = sc.nextInt();
+            int sum_b = sc.nextInt();
+            int sum_c = sum_a + sum_b ;
+            System.out.println(sum_c);
         } else if (j.equals("sub")) {
-            int q = sc.nextInt();
-            int w = sc.nextInt();
-            int r = q - w;
-            System.out.print(r);
-        } else {
+            int sub_a = sc.nextInt();
+            int sub_b = sc.nextInt();
+            int sub_c = sub_a - sub_b;
+            System.out.print(sub_c);
+        } else if (j.equals("mult"))
+        {
+            int mult_a = sc.nextInt();
+            int mult_b = sc.nextInt();
+            int mult_c = mult_a * mult_b;
+            System.out.print(mult_c); 
+        } else if (j.equals("div"))
+        { 
+            int div_a = sc.nextInt();
+            int div_b = sc.nextInt();
+            int div_c = div_a / div_b ;
+            System.out.print(div_c);
+        }
+         else {
             System.out.print("Not Valid");
         }
         sc.close();
